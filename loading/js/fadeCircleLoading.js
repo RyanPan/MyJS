83496df21b2562fbace5a391d03f7a7c906ee944
Ryan Pan <ryan.ppan@gmail.com>
2018-06-05 15:59:13 +0800

    less file reflect

--- loading/js/fadeCircleLoading.js
+++ loading/js/fadeCircleLoading.js
@@ -1,18 +1,31 @@
 (function($) {
     $.fn.fadeCircleLoading = function(options) {
+        var RunSpeed = {
+            low: 1,
+            normal: 2,
+            fast: 3
+        };
+        var FadeSpeed = {
+            low: 1,
+            normal: 2,
+            fast: 3  
+        };
+        
         // This is the easiest way to have default options.
         var settings = $.extend({
-            circleCount: 12
+            circleSize: 40,
+            runSpeed: RunSpeed.normal,
+            fadeSpeed: FadeSpeed.normal
         }, options);
         
         // Set the fade circle count based on the settings variable.
-        this.append((function(count) {
-            var arr = new Array(count);
+        this.append((function() {
+            var arr = new Array(12);
             $(arr).each(function(index) {
                 arr[index] = $("<div>");
             });
             return arr;
-        })(settings.circleCount)).addClass("sk-fading-circle");
+        })()).addClass("fadeCircleLoading");
     
         // chainable
         return this;
